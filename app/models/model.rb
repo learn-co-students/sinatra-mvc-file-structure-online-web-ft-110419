308c14c6913b493589c2d765727e641643d07e8c
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-08 00:01:10 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,11 +1,15 @@
-class Model
-
-end
-class Dog 
-  attr_accessor :name,:breed,:age
-  def initialize(name,breed,age)
+class Dog
+  attr_accessor :name, :breed, :age
+  @@dogs = []
+  def initialize(name = nil,breed=nil,age=nil)
     @name = name
     @breed = breed
     @age = age
+    @@dogs << self
+  end
+  def self.all
+
+    @@dogs
   end
 end
+
